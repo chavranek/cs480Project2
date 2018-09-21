33ccd394c4d95b85011cd702e7fe956741633056
Christian Havranek <havranek@sonoma.edu>
2018-09-21 13:46:50 -0700

    master

--- proj2.cpp
+++ proj2.cpp
@@ -1,3 +1,8 @@
+/*
+Authors: Christian Havranek, Christopher Tran, Cole Watson
+Project: Project 2 (BFS and IDS with permutation)
+ */
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -10,8 +15,12 @@ int main()
   vector<int> permutation;
   cout << "Input a permutation: " << endl;
 
+  // grabs the entire line of user input
   getline(cin, P);
-  
+
+  // moves through the entire user input and
+  // converts all characters to ints, then
+  // pushes them onto a vector.
   for(int i = 0; i < P.length(); i++)
     {
       if (isdigit(P[i]))
@@ -21,7 +30,8 @@ int main()
 	  permutation.push_back(num);
 	}
     }
-  
+
+  // just to make sure the vector is correct
   for(int i = 0; i < permutation.size(); i++)
     {
       cout << permutation[i] << " ";
